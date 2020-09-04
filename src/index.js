9bcf9456d3eec38f7edf83675a68a8f95dff15d7
Djordje Savanovic <ds@hydrogrid.eu>
2020-09-04 10:41:47 +0200

    - Connect & initialize Firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase";
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBS4zF-KPrqs4NGBR2gbhKr1ximGLM_ArI",
+    authDomain: "starwarsvotingapp.firebaseapp.com",
+    databaseURL: "https://starwarsvotingapp.firebaseio.com",
+    projectId: "starwarsvotingapp",
+    storageBucket: "starwarsvotingapp.appspot.com",
+    messagingSenderId: "301888680987",
+    appId: "1:301888680987:web:4ddf50f83f38767074f4be"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <React.StrictMode>
