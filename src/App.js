8b5458712a7370b3775b83b1ca9ff97aeede583c
Djordje Savanovic <ds@hydrogrid.eu>
2020-09-04 16:34:20 +0200

    - WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            people: []
+        }
+    }
+
+    getData() {
+        const ref = firebase.database().ref('/people')
+
+        ref.on("value", snapshot => {
+            const state = snapshot.val();
+            const people = state.filter(Boolean);
+            this.setState({people: people}, () => this.state.people.map(item => this.getCharacters(item.id)));
+        });
+    }
+
+    getCharacters(id) {
+        fetch(`https://swapi.dev/api/people/${id}/`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result)
+                },
+                (error) => {
+                    console.log(error)
+                }
+            )
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    handleVote(type, id) {
+        const elementsIndex = this.state.people.findIndex((element) => element.id === id )
+        let newArray = [...this.state.people]
+
+        if (type === 'yay') {
+            newArray[elementsIndex] = {...newArray[elementsIndex], yay: newArray[elementsIndex].yay + 1}
+        }
+
+        if (type === 'nay') {
+            newArray[elementsIndex] = {...newArray[elementsIndex], nay: newArray[elementsIndex].nay + 1}
+        }
+
+        const ref = firebase.database().ref('/people')
+
+        ref.update(newArray)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.people.map((person) => {
+                        return (
+                            <div style={{marginBottom: '10px'}} key={person.id}>
+                                <button value={!person ? 0 : person.yay} onClick={() => this.handleVote('yay', person.id)}>YAY: {!person ? 0 : person.yay}</button>
+                                <button value={!person ? 0 : person.nay} onClick={() => this.handleVote('nay', person.id)}>NAY: {!person ? 0 : person.nay}</button> <br />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
